5e266227647702a3f7d18a4eb5e1e16d4d7836ce
Felipe Martins <felipemjesus@outlook.com>
2020-10-16 15:59:45 -0300

    Criado a navegação entre páginas

--- App.tsx
+++ App.tsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import MapView, { PROVIDER_GOOGLE, Marker, Callout } from 'react-native-maps';
-import { Feather } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
-
-import mapMarker from './src/images/map-marker.png';
+import { StatusBar } from 'expo-status-bar';
+import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -20,99 +16,9 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <MapView 
-        provider={PROVIDER_GOOGLE}
-        style={styles.map} 
-        initialRegion={{
-          latitude: -15.649944,
-          longitude: -47.786250,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker 
-          icon={mapMarker}
-          calloutAnchor={{
-            x: 2.7,
-            y: 0.8,
-          }}
-          coordinate={{
-            latitude: -15.649944,
-            longitude: -47.786250,
-          }}
-        >
-          <Callout tooltip onPress={() => alert('oi')}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Lar das menidas</Text>
-            </View>
-          </Callout>
-        </Marker>
-      </MapView>
-
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
-        
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
-          <Feather name="plus" size={20} color="#fff" />
-        </TouchableOpacity>
-      </View>
-    </View>
+    <>
+      <StatusBar style="dark" />
+      <Routes />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height,
-  },
-
-  calloutContainer: {
-    width: 160,
-    height: 46,
-    paddingHorizontal: 16,
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    borderRadius: 16,
-    justifyContent: 'center',
-  },
-
-  calloutText: {
-    color: '#0089a5',
-    fontSize: 14,
-    fontFamily: 'Nunito_700Bold',
-  },
-
-  footer: {
-    position: 'absolute',
-    left: 24,
-    right: 24,
-    bottom: 32,
-    backgroundColor: '#fff',
-    borderRadius: 20,
-    height: 56,
-    paddingLeft: 24,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    elevation: 10,
-  },
-
-  footerText: {
-    color: '#8fa7b3',
-    fontFamily: 'Nunito_700Bold',
-  },
-
-  createOrphanageButton: {
-    width: 56,
-    height: 56,
-    backgroundColor: '#15c3d6',
-    borderRadius: 20,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
